75f21dc76dd397cf7756fce6bff7d2fc7f6536a0
Ryan <2689297539@qq.com>
2021-05-14 12:56:21 +0800

    first commit

---
+++ src/main/java/com/attoday/GItTest.java
@@ -0,0 +1,19 @@
+package com.attoday;
+/**
+ * @author He Zhanhong
+ * @create 2021-05-14-下午 12:46
+ */
+
+/**
+ *@ClassName GItTest
+ *@Description
+ *@Author HeZhanhong
+ *@Date 2021/5/14 0014下午 12:46
+ *@Version 1.0
+ */
+public class GItTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
