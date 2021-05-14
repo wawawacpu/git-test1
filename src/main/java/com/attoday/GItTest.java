2cbd306d770bb2c249e7a4406cc80421400cc0fe
Ryan <2689297539@qq.com>
2021-05-14 13:15:11 +0800

    hot-fix commit

--- src/main/java/com/attoday/GItTest.java
+++ src/main/java/com/attoday/GItTest.java
@@ -16,5 +16,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git2!");
+        System.out.println("Hello hot-fix!");
     }
 }
